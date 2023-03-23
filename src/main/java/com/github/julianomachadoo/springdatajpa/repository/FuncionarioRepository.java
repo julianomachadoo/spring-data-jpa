9061b98bebd80765cce38b9fa558ccc0eedee1c0
julianomachadoo <julmachadoramos@gmail.com>
2023-03-23 10:37:17 -0300

    consulta com jpql

--- src/main/java/com/github/julianomachadoo/springdatajpa/repository/FuncionarioRepository.java
+++ src/main/java/com/github/julianomachadoo/springdatajpa/repository/FuncionarioRepository.java
@@ -1,12 +1,16 @@
 package com.github.julianomachadoo.springdatajpa.repository;
 
 import com.github.julianomachadoo.springdatajpa.orm.Funcionario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
     List<Funcionario> findByNome(String nome);
+    @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.dataContratacao = :data")
+    List<Funcionario> findNomeDataContratacaoSalarioMaior(String nome, Double salario, LocalDate data);
 }
--- src/main/java/com/github/julianomachadoo/springdatajpa/service/RelatoriosService.java
+++ src/main/java/com/github/julianomachadoo/springdatajpa/service/RelatoriosService.java
@@ -1,11 +1,11 @@
 package com.github.julianomachadoo.springdatajpa.service;
 
-import com.github.julianomachadoo.springdatajpa.orm.Cargo;
 import com.github.julianomachadoo.springdatajpa.orm.Funcionario;
-import com.github.julianomachadoo.springdatajpa.repository.CargoRepository;
 import com.github.julianomachadoo.springdatajpa.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class RelatoriosService {
 
     private boolean system = true;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private final FuncionarioRepository funcionarioRepository;
 
     public RelatoriosService(FuncionarioRepository funcionarioRepository) {
@@ -26,11 +27,13 @@ public class RelatoriosService {
             System.out.println("Qual ação de cargo deseja executar?");
             System.out.println("0 - Sair");
             System.out.println("1 - Busca funcionario por nome");
+            System.out.println("2 - Busca funcionario por nome, data contratacao e salario maior");
 
             int action = scanner.nextInt();
 
             switch (action) {
                 case 1 -> buscaFuncionarioPorNome(scanner);
+                case 2 -> buscaFuncionarioNomeSalarioMaiorData(scanner);
                 default -> system = false;
             }
         }
@@ -41,5 +44,21 @@ public class RelatoriosService {
         String nome = scanner.next();
         List<Funcionario> list = funcionarioRepository.findByNome(nome);
         list.forEach(System.out::println);
+    }
+
+    private void buscaFuncionarioNomeSalarioMaiorData(Scanner scanner) {
+        System.out.println("Qual nome deseja pesquisar?");
+        String nome = scanner.next();
+
+        System.out.println("Qual data de contratacao deseja pesquisar?");
+        String dataContratacao = scanner.next();
+        LocalDate localDate = LocalDate.parse(dataContratacao, formatter);
+
+        System.out.println("Qual salario deseja pesquisar?");
+        Double salario = scanner.nextDouble();
+
+        List<Funcionario> funcionarios = funcionarioRepository
+                .findNomeDataContratacaoSalarioMaior(nome, salario, localDate);
+        funcionarios.forEach(System.out::println);
     }
 }
