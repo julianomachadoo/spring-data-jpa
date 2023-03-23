ae127c9c327e80def494aa60af6a87b911bcabcc
julianomachadoo <julmachadoramos@gmail.com>
2023-03-23 14:47:35 -0300

    Compondo Specifications

--- src/main/java/com/github/julianomachadoo/springdatajpa/service/RelatorioFuncionarioDinamico.java
+++ src/main/java/com/github/julianomachadoo/springdatajpa/service/RelatorioFuncionarioDinamico.java
@@ -4,12 +4,17 @@ import com.github.julianomachadoo.springdatajpa.orm.Funcionario;
 import com.github.julianomachadoo.springdatajpa.repository.FuncionarioRepository;
 import com.github.julianomachadoo.springdatajpa.specification.SpecificationFuncionario;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
+@Service
 public class RelatorioFuncionarioDinamico {
 
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private final FuncionarioRepository funcionarioRepository;
 
     public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
@@ -20,7 +25,42 @@ public class RelatorioFuncionarioDinamico {
         System.out.println("Digite um nome");
         String nome = scanner.next();
 
+        if (nome.equalsIgnoreCase("NULL")) {
+            nome = null;
+        }
+
+        System.out.println("Digite um cpf");
+        String cpf = scanner.next();
+
+        if (cpf.equalsIgnoreCase("NULL")) {
+            cpf = null;
+        }
+
+        System.out.println("Digite um salario");
+        Double salario = scanner.nextDouble();
+
+        if (salario == 0) {
+            salario = null;
+        }
+
+        System.out.println("Digite uma data");
+        String data = scanner.next();
+
+        LocalDate dataContratacao;
+
+        if (data.equalsIgnoreCase("NULL")) {
+            dataContratacao = null;
+        } else {
+            dataContratacao = LocalDate.parse(data, formatter);
+        }
+
         List<Funcionario> funcionarios = funcionarioRepository
-                .findAll(Specification.where(SpecificationFuncionario.nome(nome)));
+                .findAll(Specification
+                        .where(SpecificationFuncionario.nome(nome))
+                        .or(SpecificationFuncionario.cpf(cpf))
+                        .or(SpecificationFuncionario.salario(salario))
+                        .or(SpecificationFuncionario.dataContratacao(dataContratacao))
+                );
+        funcionarios.forEach(System.out::println);
     }
 }
--- src/main/java/com/github/julianomachadoo/springdatajpa/specification/SpecificationFuncionario.java
+++ src/main/java/com/github/julianomachadoo/springdatajpa/specification/SpecificationFuncionario.java
@@ -3,9 +3,27 @@ package com.github.julianomachadoo.springdatajpa.specification;
 import com.github.julianomachadoo.springdatajpa.orm.Funcionario;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDate;
+
 public class SpecificationFuncionario {
 
     public static Specification<Funcionario> nome(String nome) {
-        return (root, criterioQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+    }
+
+    public static Specification<Funcionario> cpf(String cpf) {
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("cpf"), "%" + cpf + "%");
+    }
+
+    public static Specification<Funcionario> salario(Double salario) {
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.greaterThan(root.get("salario"), "%" + salario + "%");
+    }
+
+    public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao) {
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.greaterThan(root.get("nome"), "%" + dataContratacao + "%");
     }
 }
